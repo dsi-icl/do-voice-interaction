66d6663764ab89f55f1fb1012329ad7ed122f38a
Aurélie <aurelie.beaugeard@insa-rouen.fr>
2020-07-30 21:23:59 +0200

    Documentation added, see out named-folder

--- gdo_voicebot/stt_service/src/routes/index.js
+++ gdo_voicebot/stt_service/src/routes/index.js
@@ -1,7 +1,24 @@
+/**
+ * @file Manages some utilities functions
+ * @author Aurélie Beaugeard
+*/
+
+/**
+ * This function returns a json response to be sent at the /api/json route.
+ * 
+ * @param {Request} req The user's request
+ * @param {Response} res The service response
+ */
 export function sampleData(req, res) {
     res.json({data: "sampleData"});
 }
 
+/**
+ * This function checks if any object is empty.
+ * 
+ * @param {Object} obj The object to be checked 
+ * @returns {Boolean} The boolean value
+ */
 export function isEmpty(obj) {
     for(var key in obj) {
         if(Object.prototype.hasOwnProperty.call(obj,key))
