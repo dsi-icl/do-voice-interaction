32cec1625053721e21cbf9f4ccecf429846e3c9c
Aurélie <aurelie.beaugeard@insa-rouen.fr>
2020-07-31 14:55:49 +0200

    deepspeech bug resolved and new documentation

--- gdo_voicebot/voice_assistant_service/public/javascripts/index.js
+++ gdo_voicebot/voice_assistant_service/public/javascripts/index.js
@@ -1,5 +1,19 @@
+/**
+ * @file Manages utils functions used by the client
+ * @author Aurélie Beaugeard
+*/
+
 var hark = require("../hark.ks");
 
+/**
+ * Function that instanciated and return a hark object
+ * 
+ * @param {Stream} stream The navigator stream
+ * @param {Options} options The options that can be added : interval, threshold, play, audioContext
+ * @returns {Hark} The hark object
+ * @see{@link https://www.npmjs.com/package/hark|hark}
+ * @see{@link https://github.com/otalk/hark|Hark}
+ */
 function Hark(stream,options){
     return hark(stream,options);
 }
