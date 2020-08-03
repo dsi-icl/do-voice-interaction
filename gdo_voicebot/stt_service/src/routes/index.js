345c39088d1e0a2b461f416dc51fde616b4617f7
Aurélie <aurelie.beaugeard@insa-rouen.fr>
2020-08-03 14:11:32 +0200

    code cleaning + syntax checked + models parameters in config.json + deepspeech model updated

--- gdo_voicebot/stt_service/src/routes/index.js
+++ gdo_voicebot/stt_service/src/routes/index.js
@@ -5,24 +5,23 @@
 
 /**
  * This function returns a json response to be sent at the /api/json route.
- * 
+ *
  * @param {Request} req The user's request
  * @param {Response} res The service response
  */
-export function sampleData(req, res) {
-    res.json({data: "sampleData"});
+export function sampleData (req, res) {
+  res.json({ data: 'sampleData' })
 }
 
 /**
  * This function checks if any object is empty.
- * 
- * @param {Object} obj The object to be checked 
+ *
+ * @param {Object} obj The object to be checked
  * @returns {Boolean} The boolean value
  */
-export function isEmpty(obj) {
-    for(var key in obj) {
-        if(Object.prototype.hasOwnProperty.call(obj,key))
-            return false;
-    }
-    return true;
+export function isEmpty (obj) {
+  for (var key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) { return false }
+  }
+  return true
 }
