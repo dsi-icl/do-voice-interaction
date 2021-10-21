831f059d7a1325586ef21004e35254b7f81a5512
izabellakacprzak <izabella.kacprzak@gmail.com>
2021-10-21 18:21:27 +0100

    [IK] deleted old named error correction folder

--- gdo_voicebot/grammar_correction_service/run.py
+++ gdo_voicebot/grammar_correction_service/run.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, request
+import json
+import spacy
 
 app = Flask(__name__)
+nlp = spacy.load("en_core_web_sm")
 
+@app.route("/error-correction", methods=['POST'])
+def tag_parts_of_speech():
+    receivedData = json.loads(request.data)
+    text_data = receivedData['transcript']
+    doc = nlp(text_data)
+
+    return doc
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=9000, debug=False) # set debug=False for production
