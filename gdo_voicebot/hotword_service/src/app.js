1a7766ebfd017db40cd7dcfec813b6c1fd862dd9
vlad <vn119@ic.ac.uk>
2021-10-28 11:54:52 +0100

    [vn119] Adds and pipes Writable Stream to KeywordClient. Enables audio blob transfers in app.js

--- gdo_voicebot/hotword_service/src/app.js
+++ gdo_voicebot/hotword_service/src/app.js
@@ -1,13 +1,34 @@
 import express from 'express'
 // import cors from 'cors'
+// import logger from 'morgan'
 
-import { startListening } from './routes/hotword.js'
+import { startListening, getHotword } from './routes/hotword.js'
 
 const app = express()
 
 // app.use(cors())
+// app.use(logger('dev'))
+/* Has been added to enable audio blob transfers */
+app.use(express.text({ limit: '50mb' }))
+app.use(express.urlencoded({ extended: false }))
 
+// app.get('/', getHotword)
+
+/**
+ * Get service status
+ *
+ * @name Getting service status
+ * @route {GET} /api/status
+ */
 app.get('/api/status', (req, res) => res.status(200).json({ status: 'ok', service: 'Hotword service', text: "Hotword Service Active" }))
-app.post('/api/hotword', (req, res) => startListening(req, res))
+
+/**
+ * Listen for hotword using the received audio blob
+ *
+ * @name Start-Listening notify on hotword detection
+ * @route {POST} /api/hotword
+ * @headerparam Content-type must be text/plain
+ */
+app.post('/api/hotword', (req, res) => { startListening(req, res) })
 
 export default app
--- gdo_voicebot/hotword_service/src/routes/hotword.js
+++ gdo_voicebot/hotword_service/src/routes/hotword.js
@@ -1,12 +1,17 @@
 import WakewordDetector from '@mathquis/node-personal-wakeword'
+import { ReadStream } from 'fs'
+// import { Readable } from 'stream'
+import Stream from 'stream'
 
-const keywordClient = new WakewordDetector({
-    sampleRate: 48000,
-    threshold: 0.5
-})
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fileSystem from 'fs'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export async function startListening(req, res) {
-	if (req.body === null) {
+	if (req.body === null || req.body === undefined) {
 		res.status(400).json({
 			status: 'fail',
 			service: 'Hotword service',
@@ -15,53 +20,92 @@ export async function startListening(req, res) {
 		return
 	}
 
-	getHotword(req.body)
-	keywordClient.emit('ready')
-
-	console.log('Hotword Service started successfully')
-	res.status(200).json({ status: 'ok', service: 'Hotword service', text: 'Hotword Service started successfully'})
+	// console.log("Body checks passed")
+	getHotword(req.body, res)
+	// keywordClient.emit('ready')
 }
 
-async function getHotword(audioData) {
+export async function getHotword(audioData, res) {
+	const keywordClient = new WakewordDetector({
+		// sampleRate: 16000,
+		threshold: 0.5
+	})
+
+	// Define keywords
+	await keywordClient.addKeyword('heyGalileo', [
+	  './keywords/heyGalileo1.wav',
+		'./keywords/heyGalileo2.wav',
+		'./keywords/heyGalileo3.wav',
+		'./keywords/heyGalileo4.wav',
+		'./keywords/heyGalileo5.wav',
+		'./keywords/heyGalileo6.wav',
+		'./keywords/heyGalileo7.wav',
+		'./keywords/heyGalileo8.wav',
+		'./keywords/heyGalileo9.wav',
+	  './keywords/heyGalileo10.wav'
+	], {
+	  disableAveraging: true,
+	  threshold: 0.52
+	})
+
+	keywordClient.enableKeyword('heyGalileo')
+
 	keywordClient.on('ready', () => {
 		console.log('Listening for hotword...')
 	})
+
+	// The detector will emit a "vad-silence" event when no voice is heard
+  keywordClient.on('vad-silence', () => {
+    console.log('Hearing silence...')
+  })
+
+  // The detector will emit a "vad-voice" event when it hears a voice
+  keywordClient.on('vad-voice', () => {
+    console.log('Hearing voices...')
+  })
 
 	keywordClient.on('error', err => {
 		console.error(err.stack)
 	})
 
-	keywordClient.on('keyword', ({keyword, score, threshold, timestamp, audioData}) => {
+	keywordClient.on('keyword', ({keyword, score, threshold, timestamp}) => {
 		console.log(`Detected "${keyword}" with score ${score} / ${threshold}`)
 		// make api call to STT
 	})
 
-	// Define keywords
-	// await keywordClient.addKeyword('abrakadabra', [
-	//     './keywords/abrakadabra1.wav',
-	//     './keywords/abrakadabra2.wav'
-	// ], {
-	//     disableAveraging: true,
-	//     threshold: 0.52
-	// })
+	const detectionStream = new Stream.Writable({
+    objectMode: true,
+    write: (data, enc, done) => {
+      console.log(data)
+      done()
+    }
+  })
 
-	// keywordClient.enableKeyword('abrakadabra')
+  keywordClient.pipe(detectionStream)
 
-	// const buffer = Buffer.from(req.body, 'base64')
-	
-	// keywordClient.once('ready', () => {
-	// 	console.log('Discord Client Loaded!');
-	// });
+	const filePath = path.resolve(__dirname, './keywords', './heyGalileo1.wav');
+	const readStream = fileSystem.createReadStream(filePath);
+	readStream.pipe(keywordClient)
+	console.log(readStream)
 
-	// const context = new AudioContext();
-	// const source = context.createMediaStreamSource(stream);
-	// const processor = context.createScriptProcessor(1024, 1, 1);
+	const buffer = Buffer.from(audioData, 'base64')
+	const readable = bufferToStream(buffer);
+	// const readable = new Stream.Readable()
+	// readable._read = () => {} // _read is required but you can noop it
+	// readable.push(buffer)
+	// // readable.push(null)
+	// readable.pipe(keywordClient)
+	console.log(readable)
 
-	// source.connect(processor);
-	// processor.connect(context.destination);
+	console.log('Hotword Service started successfully')
+	setTimeout(() => {
+		res.status(200).json({ status: 'ok', service: 'Hotword service', text: 'Hotword Service responded successfully'})
+  }, 5000)
+}
 
-	// processor.onaudioprocess = function(e) {
-	//   // Do something with the data, e.g. convert it to WAV
-	//   console.log(e.inputBuffer);
-	// };
+function bufferToStream(myBuuffer) {
+    let tmp = new Stream.Duplex();
+    tmp.push(myBuuffer);
+    tmp.push(null);
+    return tmp;
 }
